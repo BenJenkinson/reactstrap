38cc8ca0aacfaed782cc8e7a26e0331715480287
Ben Jenkinson <59176+BenJenkinson@users.noreply.github.com>
2022-06-08 18:11:35 +0100

    fix(#2425) Prefer new `.text-bg-{color}` helpers over `.bg-{color}` (types)

--- types/lib/Badge.d.ts
+++ types/lib/Badge.d.ts
@@ -3,7 +3,21 @@ import { CSSModule } from './utils';
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLElement> {
   [key: string]: any;
+  /**
+   * Sets the background color with contrasting text color
+   * @see https://getbootstrap.com/docs/5.2/helpers/color-background/
+   */
   color?: string;
+  /**
+   * Sets the background color only (Alternatively, set background & text color with `color`)
+   * @see https://getbootstrap.com/docs/5.2/utilities/background/
+   */
+  bgColor?: string;
+  /**
+   * Sets the text color only (Alternatively, set background & text color with `color`)
+   * @see https://getbootstrap.com/docs/5.2/utilities/colors/
+   */
+  textColor?: string;
   pill?: boolean;
   tag?: React.ElementType;
   innerRef?: React.Ref<HTMLElement>;
